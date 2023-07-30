db7d66c942d7141a25df287372a0b6f29c570140
Gabriel Cedraz <69488235+cedrazgabriel@users.noreply.github.com>
2023-07-30 14:08:43 -0300

    create front end project and implement route logic

--- api/src/shared/config/env.ts
+++ api/src/shared/config/env.ts
@@ -1,25 +1,24 @@
-import { plainToInstance } from 'class-transformer'
-import { IsNotEmpty, IsString, NotEquals, validateSync } from "class-validator"
+import { plainToInstance } from 'class-transformer';
+import { IsNotEmpty, IsString, NotEquals, validateSync } from 'class-validator';
 
 class Env {
-    @IsString()
-    @IsNotEmpty()
-    dbURL: string
+  @IsString()
+  @IsNotEmpty()
+  dbURL: string;
 
-    @IsString()
-    @IsNotEmpty()
-    @NotEquals('unsecure_jwt_secret')
-    jwtSecret: string
+  @IsString()
+  @IsNotEmpty()
+  @NotEquals('unsecure_jwt_secret')
+  jwtSecret: string;
 }
 
-export const env: Env = plainToInstance(Env,{
-    dbURL: process.env.DATABASE_URL,
-    jwtSecret: process.env.JWT_SECRET,
-})
+export const env: Env = plainToInstance(Env, {
+  dbURL: process.env.DATABASE_URL,
+  jwtSecret: process.env.JWT_SECRET,
+});
 
- const errors = validateSync(env)
+const errors = validateSync(env);
 
- if(errors.length > 0){
-    throw new Error(JSON.stringify(errors,null,2))
- }
-   
+if (errors.length > 0) {
+  throw new Error(JSON.stringify(errors, null, 2));
+}
